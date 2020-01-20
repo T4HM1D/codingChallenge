87582e6f5bddc8b1f6851c90d4c81fb7229786e4
Tahmidol Alom <32310776+T4HM1D@users.noreply.github.com>
2020-01-20 11:52:16 +0000

    youtube audio downloader

---
+++ challenge2/downloader.py
@@ -0,0 +1,31 @@
+from __future__ import unicode_literals
+import youtube_dl
+import os
+from sys import argv
+
+# Downloads data and configs
+download_options = {
+	'format' : 'bestaudio/best',
+	'outtmpl': '%(title)s.%(ext)s',
+	'nocheckcertificate': True,
+	'postprocessors': [{
+		'key': 'FFmpegExtractAudio',
+		'preferredcodec': 'mp3',
+		'preferredquality': '192',
+	}],
+
+}
+
+# song directory
+if not os.path.exists('songs'):
+	os.mkdir('Songs')
+else:
+	os.chdir('Songs')
+
+# download songs
+with youtube_dl.YoutubeDL(download_options) as dl:
+	with open('../' + argv[1], 'r') as f:
+		for song_url in f:
+			dl.download([song_url])
+
+ 
